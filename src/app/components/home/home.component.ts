1add08346669345006c6d66104ba128a3ac7c9ab
eslamzayed22 <eslamzayed765@gmail.com>
2025-03-27 04:05:14 +0200

    add animations - change cursor

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { trigger, style, transition, animate } from '@angular/animations';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -6,8 +7,20 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  animations: [
+    trigger('slideFadeDown', [
+      transition(':enter', [
+        style({ transform: 'translateY(-50px)', opacity: 0 }), // يبدأ من الأعلى وغير مرئي
+        animate('0.8s ease-out', style({ transform: 'translateY(0)', opacity: 1 })) // يتحرك للأسفل ويصبح مرئيًا
+      ])
+    ]),
+    trigger('slideFadeUp', [
+      transition(':enter', [
+        style({ transform: 'translateY(50px)', opacity: 0 }), // يبدأ من الأعلى وغير مرئي
+        animate('0.8s ease-out', style({ transform: 'translateY(0)', opacity: 1 })) // يتحرك للأسفل ويصبح مرئيًا
+      ])
+    ])
+  ]
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
