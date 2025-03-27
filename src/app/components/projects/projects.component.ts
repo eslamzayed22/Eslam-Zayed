0666abb8f8f0d42a5ab81562ae6272cab8af0c69
eslamzayed22 <eslamzayed765@gmail.com>
2025-03-27 06:24:49 +0200

    add animation to projects component

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,3 +1,4 @@
+import { trigger, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
@@ -6,7 +7,21 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
+  animations: [
+      trigger('slideFadeLeft', [
+        transition(':enter', [
+          style({ transform: 'translatex(-150px)', opacity: 0 }), // يبدأ من الأعلى وغير مرئي
+          animate('0.8s ease-out', style({ transform: 'translateY(0)', opacity: 1 })) // يتحرك للأسفل ويصبح مرئيًا
+        ])
+      ]),
+      trigger('slideFadeRight', [
+        transition(':enter', [
+          style({ transform: 'translatex(150px)', opacity: 0 }), // يبدأ من الأعلى وغير مرئي
+          animate('0.8s ease-out', style({ transform: 'translateY(0)', opacity: 1 })) // يتحرك للأسفل ويصبح مرئيًا
+        ])
+      ])
+    ]
 })
 export class ProjectsComponent {
 
